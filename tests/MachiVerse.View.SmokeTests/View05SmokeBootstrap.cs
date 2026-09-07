a29e9d83f20f822458791b4637281ab30fdda722
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-07 14:14:43 +0900

    test(view): run VIEW-05 smoke suite

---
+++ tests/MachiVerse.View.SmokeTests/View05SmokeBootstrap.cs
@@ -0,0 +1,7 @@
+using System.Runtime.CompilerServices;
+
+internal static class View05SmokeBootstrap
+{
+    [ModuleInitializer]
+    internal static void Run() => View05ParticipationSmoke.Run();
+}
