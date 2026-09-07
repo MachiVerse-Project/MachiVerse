61f8f569d9eb4c30827bfa3d9846ef1fe3133c82
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-08 07:56:58 +0900

    integration(alpha): register binding operation and preference catalog

--- src/MachiVerse.View/Program.cs
+++ src/MachiVerse.View/Program.cs
@@ -18,9 +18,17 @@ var viewConfig = GeneralViewConfigLoader.LoadText(configText);
 
 builder.Services.AddSingleton(viewConfig);
 builder.Services.AddSingleton(SceneProjectionAdapterRegistry.Empty);
-builder.Services.AddSingleton(ViewOperationCatalog.Empty);
-builder.Services.AddSingleton(ParticipationPreferenceCatalog.Empty);
-builder.Services.AddSingleton(AbsencePolicyProfileCatalog.Empty);
+builder.Services.AddSingleton(new ViewOperationCatalog([
+    new ViewOperationDescriptor(
+        "participation.binding.create",
+        "operation.participation.binding.create",
+        1,
+        0),
+]));
+builder.Services.AddSingleton(new ParticipationPreferenceCatalog([
+    new ParticipationPreferenceProfile("alpha.default", Array.Empty<string>()),
+]));
+builder.Services.AddSingleton(new AbsencePolicyProfileCatalog(["alpha.default"]));
 builder.Services.AddSingleton<IParticipationOperationPayloadAdapter, UnavailableParticipationOperationPayloadAdapter>();
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<GatewayProtocolClient>();
