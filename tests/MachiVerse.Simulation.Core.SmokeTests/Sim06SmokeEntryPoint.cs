652a7afdbac1a989074861e4942712cafc471ecb
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-07 14:40:42 +0900

    chore(sim-06): use single smoke registration entrypoint

--- tests/MachiVerse.Simulation.Core.SmokeTests/Sim06SmokeEntryPoint.cs
+++
@@ -1,8 +0,0 @@
-using System.Runtime.CompilerServices;
-
-internal static class Sim06SmokeEntryPoint
-{
-    [ModuleInitializer]
-    internal static void Initialize()
-        => Sim06StepCoordinatorSmoke.RunAsync().GetAwaiter().GetResult();
-}
