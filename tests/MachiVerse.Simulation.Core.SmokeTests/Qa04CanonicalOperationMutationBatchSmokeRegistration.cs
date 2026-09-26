23b33f00b78910fbc3e59fc7a4218686c5c6b2a9
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-26 19:01:14 +0900

    test: #548 cover prevalidated partition binder equivalence

--- tests/MachiVerse.Simulation.Core.SmokeTests/Qa04CanonicalOperationMutationBatchSmokeRegistration.cs
+++ tests/MachiVerse.Simulation.Core.SmokeTests/Qa04CanonicalOperationMutationBatchSmokeRegistration.cs
@@ -3,5 +3,9 @@ using System.Runtime.CompilerServices;
 internal static class Qa04CanonicalOperationMutationBatchSmokeRegistration
 {
     [ModuleInitializer]
-    internal static void Register() => Qa04CanonicalOperationMutationBatchSmoke.Run();
+    internal static void Register()
+    {
+        Qa04CanonicalOperationMutationBatchSmoke.Run();
+        Qa04CanonicalOperationPrevalidatedPartitionSmoke.Run();
+    }
 }
